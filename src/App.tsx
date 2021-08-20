73b7365465c69b82a9bb36cc250e8dd2b1071156
Heron Maioli <heronmaioli@hotmail.com>
2021-08-20 00:17:53 -0300

    Estrutura do formulario

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
 import { GlobalStyles } from './styles/global'
+import Modal from 'react-modal'
 
 import { Dashboard } from './components/Dashboard'
 import { Header } from './components/Header'
+import { useState } from 'react'
+import { NewTransactionModal } from './components/NewTransactionModal'
 
+Modal.setAppElement('#root')
 export function App() {
-  return (
-    <>
-      <GlobalStyles />
-      <Header />
-      <Dashboard />
-    </>
-  )
+   const [isNewTransactionModalOpen, setIsNewTransactionMoldaOpen] = useState(false)
+
+   function handleOpenNewTransactionModal() {
+      setIsNewTransactionMoldaOpen(true)
+   }
+   function handleCloseNewTransactionModal() {
+      setIsNewTransactionMoldaOpen(false)
+   }
+
+   return (
+      <>
+         <GlobalStyles />
+         <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+         <NewTransactionModal
+            isOpen={isNewTransactionModalOpen}
+            onRequestClose={handleCloseNewTransactionModal}
+         />
+
+         <Dashboard />
+      </>
+   )
 }
